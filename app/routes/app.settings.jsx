ab527f9ba9bd5bafbc4721b21e108eb5bd236010
Kribas <carlousrimal@gmail.com>
2025-06-02 13:50:44 +0545

    created about and settings routes and form data handling

---
+++ app/routes/app.settings.jsx
@@ -0,0 +1,88 @@
+import {
+  Box,
+  Card,
+  Layout,
+  Link,
+  List,
+  Page,
+  Text,
+  BlockStack,
+  InlineGrid,
+  TextField,
+  Divider,
+  useBreakpoints,
+  Button,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+import { useState } from "react";
+import {useLoaderData, Form } from "@remix-run/react";
+import {data} from '@remix-run/node'
+export async function loader() {
+  //Get data from database
+  let settings = {
+    name: "My app",
+    description: "My app description",
+  };
+  return data(settings);
+}
+
+export async function action({request}) {
+  const formData = await request.formData()
+  return data(Object.fromEntries(formData))
+}
+
+export default function Settings() {
+  const settings = useLoaderData();
+  const [formState, setFormState] = useState(settings.data);
+
+  console.log('SETTINGS', settings);
+  
+
+  return (
+    <Page>
+      <TitleBar title="Settings" />
+      <BlockStack gap={{ xs: "800", sm: "400" }}>
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: 400, sm: 0 }}
+            paddingInlineEnd={{ xs: 400, sm: 0 }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                Settings
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Update app settings and preferences
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <Form method="POST">
+            <BlockStack gap="400">
+              <TextField
+                value={formState?.name}
+                name="name"
+                onChange={(value) => {
+                  setFormState({ ...formState, name: value });
+                }}
+                label="App name"
+              />
+              <TextField
+                value={formState?.description}
+                name="description"
+                onChange={(value) => {
+                  setFormState({ ...formState, description: value });
+                }}
+                label="Description"
+              />
+
+              <Button submit={true}>Save</Button>
+            </BlockStack>
+            </Form>
+          </Card>
+        </InlineGrid>
+      </BlockStack>
+    </Page>
+  );
+}
