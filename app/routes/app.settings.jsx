a726961a34dbdbb029518d25f9a86823c3dd59d6
Kribas <carlousrimal@gmail.com>
2025-06-04 14:38:12 +0545

    update settings data using upsert on prisma and identified and fixed bugs

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -15,22 +15,38 @@ import {
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { useState } from "react";
-import {useLoaderData, Form } from "@remix-run/react";
-import {data} from '@remix-run/node'
+import { useLoaderData, Form } from "@remix-run/react";
+import { data } from "@remix-run/node";
 
 //Import prisma db
-import db from '../db.server'
-
+import db from "../db.server";
 
 export async function loader() {
   //Get data from database
-  let settings = await db.settings.findFirst()
+  let settings = await db.settings.findFirst(); 
   return data(settings);
 }
 
-export async function action({request}) {
-  const formData = await request.formData()
-  return data(Object.fromEntries(formData))
+export async function action({ request }) {
+  let settings = await request.formData();
+  settings = Object.fromEntries(settings)
+
+  await db.settings.upsert({
+    where: {
+      id: "1",
+    },
+    update: {
+      id: '1',
+      name: settings?.name ?? "",
+      description: settings?.description ?? "",
+    },
+    create: {
+      id: '1',
+      name: settings?.name ?? "",
+      description: settings?.description ?? "",
+    },
+  });
+  return data(settings);
 }
 
 export default function Settings() {
@@ -58,26 +74,26 @@ export default function Settings() {
           </Box>
           <Card roundedAbove="sm">
             <Form method="POST">
-            <BlockStack gap="400">
-              <TextField
-                value={formState?.name}
-                name="name"
-                onChange={(value) => {
-                  setFormState({ ...formState, name: value });
-                }}
-                label="App name"
-              />
-              <TextField
-                value={formState?.description}
-                name="description"
-                onChange={(value) => {
-                  setFormState({ ...formState, description: value });
-                }}
-                label="Description"
-              />
+              <BlockStack gap="400">
+                <TextField
+                  value={formState?.name}
+                  name="name"
+                  onChange={(value) => {
+                    setFormState({ ...formState, name: value });
+                  }}
+                  label="App name"
+                />
+                <TextField
+                  value={formState?.description}
+                  name="description"
+                  onChange={(value) => {
+                    setFormState({ ...formState, description: value });
+                  }}
+                  label="Description"
+                />
 
-              <Button submit={true}>Save</Button>
-            </BlockStack>
+                <Button submit={true}>Save</Button>
+              </BlockStack>
             </Form>
           </Card>
         </InlineGrid>
